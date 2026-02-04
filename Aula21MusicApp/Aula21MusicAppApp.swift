4523ea68ea3c68b464390e6b004cae2f7b3786c2
johnnydev-tech <johnnyfreirejh@hotmail.com>
2026-02-04 19:24:15 -0300

    Initial Commit

---
+++ Aula21MusicApp/Aula21MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aula21MusicAppApp.swift
+//  Aula21MusicApp
+//
+//  Created by Johnny Freire on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Aula21MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
